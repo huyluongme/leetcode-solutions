49afffa2d6db1591295c40b34c2079ac8f828de9
Huy Luong Duc <huyluongme.cs@gmail.com>
2026-04-26 16:12:42 +0000

    add(c): solve 217 contains duplicate

--- problems/00001_contains_duplicate/solution.c
+++ problems/00001_contains_duplicate/solution.c
@@ -1,13 +1,43 @@
 /*
- * Problem    :
+ * Problem    : 217. Contains Duplicate
  * Author     : Huy Luong <huyluongme.cs@gmail.com>
- * Created    :
- * Language   :
- * Difficulty :
- * Tags       :
- * Link       :
+ * Created    : 
+ * Language   : C
+ * Difficulty : Easy
+ * Tags       : Array, Hash Table, Sorting
+ * Link       : https://leetcode.com/problems/contains-duplicate/
  *
- * Time       :
- * Space      :
+ * Time       : O(nlogn)
+ * Space      : O(logn)
  */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    return *(int*)a > *(int*)b;
+}
+
+bool containsDuplicate(int* nums, int numSize) {
+    qsort(nums, numSize, sizeof(nums[0]), compare);
+
+    for (int i = 0; i < numSize - 1; ++i)
+        if (nums[i] == nums[i + 1])
+            return true;
+
+    return false;
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 1};
+    int numSize = sizeof(nums)/sizeof(nums[0]);
+
+    if (containsDuplicate(nums, numSize))
+        printf("true\n");
+    else
+        printf("false\n");
+
+    return 0;
+}
 
---
+++ problems/00001_contains_duplicate/solution1.c
@@ -0,0 +1,57 @@
+/*
+ * Problem    : 217. Contains Duplicate
+ * Author     : Huy Luong <huyluongme.cs@gmail.com>
+ * Created    : 
+ * Language   : C
+ * Difficulty : Easy
+ * Tags       : Array, Hash Table, Sorting
+ * Link       : https://leetcode.com/problems/contains-duplicate/
+ *
+ * Time       : O(n)
+ * Space      : O(n)
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define TABLE_SIZE 100003 /* load factor 0.5 */
+
+int hashFunc(int key) {
+    /* Shift the key to handle negative numbers */
+    return (int)(((long long)key + 1000000000LL) % TABLE_SIZE);
+}
+
+bool containsDuplicate(int* nums, int numSize) {
+    int hash_table[TABLE_SIZE];
+    bool bucket_used[TABLE_SIZE] = {false};
+
+    for (int i = 0; i < numSize; ++i) {
+        int index = hashFunc(nums[i]);
+
+        while (bucket_used[index]) {
+            if (hash_table[index] == nums[i])
+                return true;
+
+            /* Linear probing to find the next available slot */
+            index = (index + 1) % TABLE_SIZE;
+        }
+
+        hash_table[index] = nums[i];
+        bucket_used[index] = true;
+    }
+
+    return false;
+}
+
+int main() {
+    int nums[] = {1, 2, 3, 4};
+    int numSize = sizeof(nums)/sizeof(nums[0]);
+
+    if (containsDuplicate(nums, numSize))
+        printf("true\n");
+    else
+        printf("false\n");
+
+    return 0;
+}
+
